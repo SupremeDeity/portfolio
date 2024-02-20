104e2a9610b34cdd2c49296d3ee63bb1bfad7836
SupremeDeity <29257166+SupremeDeity@users.noreply.github.com>
2024-02-20 17:10:58 +0500

    Complete hero area

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,26 @@
+import { CrystalCanvas } from "./Components/crystal";
+
 export default function Home() {
   return (
     <div className="">
-      <main className="h-screen bg-gradient-main">
-        <div className="h-full bg-black/40 flex flex-col p-12 uppercase text-primary justify-center">
-          <span className="font-light text-[24px]">Portfolio</span>
-          <span className="font-extrabold text-[64px] leading-[64px] w-max drop-shadow-primary">Muhammad<br />Mohsin</span>
+      <main className="h-dvh bg-gradient-main">
+        <div className="h-full bg-black/60 uppercase text-primary flex justify-evenly gap-x-48">
+          <div className="flex flex-col justify-center">
+            <span className="font-light text-[24px]">Portfolio</span>
+            <span className="font-extrabold text-[64px] leading-[64px] w-max drop-shadow-primary">
+              Muhammad
+              <br />
+              Mohsin
+            </span>
+          </div>
+          <div className="w-80">
+            <CrystalCanvas />
+          </div>
         </div>
       </main>
+      <div className="h-dvh">
+        Test
+      </div>
     </div>
   );
 }
