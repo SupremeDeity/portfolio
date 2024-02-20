340819542bec38cc6b3adc77befcc605e77c4811
SupremeDeity <29257166+SupremeDeity@users.noreply.github.com>
2024-02-20 14:53:16 +0500

    Basic landing page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,18 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: "#0DCF72",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-main":
+          "linear-gradient(135deg, rgba(30,30,30,1) 0%, rgba(63,69,128,1) 25%, rgba(106,150,108,1) 50%, rgba(63,69,128,1) 75%, rgba(30,30,30,1) 100%)",
+      },
+      dropShadow(utils) {
+        return { primary: "0 0 24px #0DCF72" };
       },
     },
   },
