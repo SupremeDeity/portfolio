587b615b39077779e04a354c9a505f28dd9297d3
SupremeDeity <29257166+SupremeDeity@users.noreply.github.com>
2024-07-15 14:09:24 +0500

    Fix an issue with the last commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import { HomeMainSection } from "./Components/home_main_section";
-import { HomeAboutSection } from "./Components/home_about_section";
 import { HomeSkillsSection } from "./Components/home_skills_section";
 import { Footer } from "./Components/footer";
-import { HomeProjectsSection } from "./Components/home_projects_section";
 import { HomeStatsSection } from "./Components/home_stats_section";
 import { ProjectsSection } from "./Components/project_section";
 
@@ -11,7 +9,6 @@ export default function Home() {
     <div className="magicpattern subpixel-antialiased w-screen overflow-x-hidden overflow-y-scroll sm:snap-mandatory sm:snap-y h-screen">
       <HomeMainSection />
       <HomeStatsSection />
-      {/* <HomeAboutSection /> */}
       <HomeSkillsSection />
       <ProjectsSection />
       <Footer />
